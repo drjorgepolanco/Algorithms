f69c2de64d2ccc31e9534fc195a6e3e27494dbf1
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-15 01:37:49 -0400

    Recursive exponent in Ruby and JavaScript

--- recursion.rb
+++ recursion.rb
@@ -19,3 +19,17 @@ def fizzbuzz(n)
 end
 
 fizzbuzz(1)
+
+
+# Create the exponent method. It should take 2 inputs: a and b. It should return a 
+# to the power of b (ab). For example exp(2, 3) should return 8 (which is 2*2*2).
+
+def exp(a, b)
+  if b == 0
+    1
+  else
+    a * exp(a, b - 1)
+  end
+end
+
+puts exp(2, 3)
