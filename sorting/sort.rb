25198058d4d77f8f17f72fa1736d10b4ebe6475d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-14 22:20:04 -0400

    Array sorting complete with Ruby and JavaScript

--- sorting/sort.rb
+++ sorting/sort.rb
@@ -1,5 +1,26 @@
 module Sort
   def self.selection_sort(array)
-    array.sort
+    if array.length <= 1
+      return array
+    end
+    for x in 0...array.length
+      swap(array, x)
+    end
+    array
+  end
+
+  def self.swap(array, x)
+    smallest = array[x]
+    smallest_index = x
+    for i in x...array.length
+      if smallest > array[i]
+        smallest = array[i]
+        smallest_index = i
+      end
+    end
+    array[smallest_index] = array[x]
+    array[x] = smallest
   end
 end
+
+print Sort.selection_sort([3, 1, 5, 2, 5, -2, 1, 5, -19, 30])
--- sorting/spec/sort_spec.rb
+++ sorting/spec/sort_spec.rb
@@ -9,5 +9,23 @@ describe 'Sort' do
         expect(Sort.selection_sort([])).to eq([])
       end
     end
+
+    context "when there are negative numbers" do
+      it "returns the array sorted with negatives first" do
+        expect(Sort.selection_sort([-3, 4, -8, 5, 12])).to eq([-8, -3, 4, 5, 12])
+      end
+    end
+
+    context "when there are duplicate numbers" do
+      it "returns the array with duplicates together" do
+        expect(Sort.selection_sort([8, 4, 5, 8, 3])).to eq([3, 4, 5, 8, 8])
+      end
+    end
+
+    context "when we have a large unordered array" do
+      it "returns the array sorted" do
+        expect(Sort.selection_sort([3, 1, 5, 2, 5, -2, 1, 5, -19, 30])).to eq([-19, -2, 1, 1, 2, 3, 5, 5, 5, 30])
+      end
+    end
   end
 end
