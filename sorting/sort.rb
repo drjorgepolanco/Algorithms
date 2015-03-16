d26654e1cb4fd9ae41116b0d5feeaded722d7bc3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-16 00:47:12 -0400

    Implement merge sorting and write tests

--- sorting/sort.rb
+++ sorting/sort.rb
@@ -21,6 +21,29 @@ module Sort
     array[smallest_index] = array[x]
     array[x] = smallest
   end
+
+  def self.merge_sort(array)
+    if array.size <= 1
+      array
+    else
+      half = (array.size / 2).floor
+      first_half = merge_sort(array[0...half])
+      second_half = merge_sort(array[half..-1])
+      merge(first_half, second_half)
+    end
+  end
+
+  def self.merge(first_half, second_half)
+    if first_half.size == 0
+      second_half
+    elsif second_half.size == 0
+      first_half
+    elsif first_half[0] < second_half[0]
+      [first_half[0]] + merge(first_half[1..first_half.size], second_half)
+    else
+      [second_half[0]] + merge(first_half, second_half[1..second_half.size])
+    end
+  end
 end
 
-print Sort.selection_sort([3, 1, 5, 2, 5, -2, 1, 5, -19, 30])
+# print Sort.selection_sort([3, 1, 5, 2, 5, -2, 1, 5, -19, 30])
--- sorting/spec/sort_spec.rb
+++ sorting/spec/sort_spec.rb
@@ -10,21 +10,107 @@ describe 'Sort' do
       end
     end
 
-    context "when there are negative numbers" do
+    context "when the argument has 1 number" do
+      it "returns the same array" do
+        expect(Sort.selection_sort([1])).to eq([1])
+      end
+    end
+
+    context "when the argument has negative numbers" do
       it "returns the array sorted with negatives first" do
         expect(Sort.selection_sort([-3, 4, -8, 5, 12])).to eq([-8, -3, 4, 5, 12])
       end
     end
 
-    context "when there are duplicate numbers" do
+    context "when the argument has duplicate numbers" do
       it "returns the array with duplicates together" do
         expect(Sort.selection_sort([8, 4, 5, 8, 3])).to eq([3, 4, 5, 8, 8])
       end
     end
 
-    context "when we have a large unordered array" do
+    context "when the argument has sorted elements" do
+      it "returns a sorted array" do
+        expect(Sort.selection_sort([1, 2, 3])).to eq([1, 2, 3])
+      end
+    end
+
+    context "when the argument is large and randomized" do
       it "returns the array sorted" do
-        expect(Sort.selection_sort([3, 1, 5, 2, 5, -2, 1, 5, -19, 30])).to eq([-19, -2, 1, 1, 2, 3, 5, 5, 5, 30])
+        large_array = []
+        100.times { large_array << rand(1000) }
+        sorted_array = large_array.sort
+        expect(Sort.selection_sort(large_array)).to eq(sorted_array)
+      end
+    end
+  end
+
+  describe '.merge' do
+    context "when the arguments are empty" do
+      it "returns an empty array" do
+        expect(Sort.merge([], [])).to eq([])
+      end
+    end
+
+    context "when one of the arguments is empty" do
+      it "returns the other array" do
+        x = [3, 5, 18, 15]
+        expect(Sort.merge(x, [])).to eq(x)
+        expect(Sort.merge([], x)).to eq(x)
+      end
+    end
+
+    context "when each array has 1 element" do
+      it "returns an array with both elements sorted" do
+        expect(Sort.merge([5], [2])).to eq([2, 5])
+      end
+    end
+
+    context "when you have 2 large sorted arrays" do
+      it "returns a sorted array that is the combination of the two" do
+        x = [2, 4, 6, 8, 10, 12, 14, 16]
+        y = [1, 3, 5, 7, 9, 11, 13, 15]
+        expect(Sort.merge(x, y)).to eq((x + y).sort)
+      end
+    end
+  end
+
+  describe '.merge_sort' do
+    context "when the argument is empty" do
+      it "returns an empty array" do
+        expect(Sort.merge_sort([])).to eq([])
+      end
+    end
+
+    context "when the argument has 1 element" do
+      it "returns the same array" do
+        expect(Sort.merge_sort([1])).to eq([1])
+      end
+    end
+
+    context "when the argument has sorted elements" do
+      it "returns the same array" do
+        expect(Sort.merge_sort([1, 2, 3])).to eq([1, 2, 3])
+      end
+    end
+
+    context "when the argument has unsorted elements" do
+      it "returns a sorted array" do
+        expect(Sort.merge_sort([3, 5, 2, 1])).to eq([1, 2, 3, 5])
+      end
+    end
+
+    context "when the argument has repeating elements" do
+      it "returns a sorted array" do
+        expect(Sort.merge_sort([2, 1, 2, 4, 5])).to eq([1, 2, 2, 4, 5])     
+      end
+    end
+
+    context "when the argument is large and randomized" do
+      it "returns a sorted array" do
+        large_array = []
+        100.times { large_array << rand(1000) }
+        sorted_array = large_array.sort
+        expect(Sort.merge_sort(large_array)).to eq(sorted_array)
       end
     end
   end
